c4b0fee4ba2ac1de521cc86999a891e9ebaf17ec
Alexandrina <alaz2005tv@gmail.com>
2023-09-30 12:28:08 +0300

    4.1.7

---
+++ Stepik. Generation Python/4.1 Choise of two/4.1.7.py
@@ -0,0 +1,14 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+mini = 0
+if a > b:
+    mini = b
+else:
+    mini = a
+if mini > c:
+    mini = c
+if mini > d :
+    mini = d
+print(mini)
