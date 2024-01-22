dd1a4229c45e181a898d6025b17e6229e6a8cc7e
Alexandrina <alaz2005tv@gmail.com>
2024-01-22 12:11:35 +0300

    stepik

---
+++ Stepik. Generation Python/4.2/4.2.8.py
@@ -0,0 +1,8 @@
+stol1 = int(input())
+str1 =int(input())
+stol2 =int(input())
+str2 = int(input())
+if -1 <=(stol1 - stol2) <= 1 and -1 <=(str1 - str2) <= 1 :
+    print("YES")
+else:
+    print("NO")
